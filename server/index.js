ef3470fdfcdfc7e6f8644876545cd2b8c7bdf13f
Yaksh <229895498+yaksh-chauhan19@users.noreply.github.com>
2026-05-23 15:30:48 +0530

    fix port binding

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
     console.log('🚀 Server running at http://localhost:3000');
 });
